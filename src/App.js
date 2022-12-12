9533fca04046bfef3e170d0f3f9b8ef0d128cf05
nikhilpm1 <nikhil@hamon.in>
2022-12-12 19:48:02 +0530

    add route for homepage and bookings page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BookingListPage from './pages/BookingListPage';
 
 function App() {
   return (
     <div className="App">
-      
+       <BrowserRouter>
+                <Routes>
+                    <Route index path="/" element={<HomePage />} />
+                    <Route path="/bookings" element={<BookingListPage />} />
+                </Routes>
+            </BrowserRouter>
     </div>
   );
 }
